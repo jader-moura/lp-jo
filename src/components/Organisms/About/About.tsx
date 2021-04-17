7d5aefd6cd6a3fc4868f0774327dd302c216be90
Jader Vinícius Gonzaga Moura <60973969+jader-moura@users.noreply.github.com>
2021-04-17 06:26:24 -0300

    build test

--- src/components/Organisms/About/About.tsx
+++ src/components/Organisms/About/About.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import Button from '../../Molecules/BlackButton/BlackButton';
 import ContentWidth from '../../Molecules/ContentWidth/ContentWidth';
-import Counter from '../../Molecules/Counter/Counter';
-
-import { WhiteCorretor, BoxText, Container, CounterContent } from './styles';
+import { WhiteCorretor, BoxText, Container } from './styles';
 
 interface props {
-  subtitle: string,
-  title: string,
-  text: string,
   id: string,
 }
 
-const About = ({ subtitle, title, text, id }: props) => {
-
+const About = ({id} : props) => {
   return (
     <WhiteCorretor>
       <ContentWidth>
@@ -21,15 +15,11 @@ const About = ({ subtitle, title, text, id }: props) => {
           <img src="/image-about.jpg" />
           <section>
             <BoxText>
-              <h3>{subtitle = "Bem-Vindos"}</h3>
-              <h2>{title = "Jordana Mariano"}</h2>
-              <p>{text = "Formada em Arquitetura e Urbanismo, Jordana Mariano trabalha em carreira solo fazendo muitas vezes parcerias com colegas de profissão, elabora projetos capazes de explorar e experimentar a arquitetura na mais ampla escala possível; desde casas, escolas, edifícios religiosos, hotéis, edifícios universitários, centros de recreação, acreditando em dar a soluções únicas para oferecer o melhor aos seus clientes."}</p>
+              <h3>Bem-Vindos</h3>
+              <h2>Jordana Mariano</h2>
+              <p>Formada em Arquitetura e Urbanismo, Jordana Mariano trabalha em carreira solo fazendo muitas vezes parcerias com colegas de profissão, elabora projetos capazes de explorar e experimentar a arquitetura na mais ampla escala possível; desde casas, escolas, edifícios religiosos, hotéis, edifícios universitários, centros de recreação, acreditando em dar a soluções únicas para oferecer o melhor aos seus clientes.</p>
               <Button type="button" href="#contato" text="Entrar em contato" />
             </BoxText>
-            {/* <CounterContent>
-              <Counter countTotal={3} countText="Anos de experiência" key={1} />
-              <Counter countTotal={29} countText="Projetos criados"  key={2} />
-            </CounterContent> */}
           </section>
         </Container>
       </ContentWidth>
